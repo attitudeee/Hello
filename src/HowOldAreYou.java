162d11a5e2a54e894db5818bbe693a3d32cdcd9b
attitudeeee <22fi101@ms.dendai.ac.jp>
2024-04-19 15:15:14 +0900

    後は抜け出すコードを考えるだけ。

--- src/HowOldAreYou.java
+++ src/HowOldAreYou.java
@@ -15,33 +15,36 @@ public class HowOldAreYou {
         // readLine() は、入出力エラーの可能性がある。エラー処理がないとコンパイルできない。
         // Java では、 try{ XXXXXXXX } catch(エラーの型 変数) { XXXXXXXXXXXXXXXXXX} と書く
         try {
-            System.out.println("何歳ですか?");
-            String line = reader.readLine();
-            int age = Integer.parseInt(line);
-            if (age < 0 || age >= 120) {
-                System.out.println("年齢は0以上120未満の範囲で入力して下さい。");
-            } else {
-                System.out.println("あなたは" + age + "歳ですね。");
-                System.out.println("2030年のあなたの年齢は、" + (age + 6) + "歳ですね。");
-                if ((157 - age) < 45 & (157 - age) > 0) {
-                    System.out.println("あなたの生まれた元号は、明治" + (157 - age) + "年ですね。");
-                    System.out.println("あなたの生まれた西暦は、" + (2024 - age) + "年ですね。");
-                }
-                if ((113 - age) < 15 & (113 - age) > 0) {
-                    System.out.println("あなたの生まれた元号は、大正" + (113 - age) + "年ですね。");
-                    System.out.println("あなたの生まれた西暦は、" + (2024 - age) + "年ですね。");
-                }
-                if ((99 - age) < 64 & (99 - age) > 0) {
-                    System.out.println("あなたの生まれた元号は、昭和" + (99 - age) + "年ですね。");
-                    System.out.println("あなたの生まれた西暦は、" + (2024 - age) + "年ですね。");
-                }
-                if ((36 - age) < 31 & (36 - age) > 0) {
-                    System.out.println("あなたの生まれた元号は、平成" + (36 - age) + "年ですね。");
-                    System.out.println("あなたの生まれた西暦は、" + (2024 - age) + "年ですね。");
-                }
-                if ((6 - age) < 7 & (6 - age) > 0) {
-                    System.out.println("あなたの生まれた元号は、令和" + (6 - age) + "年ですね。");
-                    System.out.println("あなたの生まれた西暦は、" + (2024 - age) + "年ですね。");
+            while (true) {
+                // String str = reader.readLine();
+                System.out.println("何歳ですか?");
+                String line = reader.readLine();
+                int age = Integer.parseInt(line);
+                if (age < 0 || age >= 120) {
+                    System.out.println("年齢は0以上120未満の範囲で入力して下さい。");
+                } else {
+                    System.out.println("あなたは" + age + "歳ですね。");
+                    System.out.println("2030年のあなたの年齢は、" + (age + 6) + "歳ですね。");
+                    if ((157 - age) < 45 & (157 - age) > 0) {
+                        System.out.println("あなたの生まれた元号は、明治" + (157 - age) + "年ですね。");
+                        System.out.println("あなたの生まれた西暦は、" + (2024 - age) + "年ですね。");
+                    }
+                    if ((113 - age) < 15 & (113 - age) > 0) {
+                        System.out.println("あなたの生まれた元号は、大正" + (113 - age) + "年ですね。");
+                        System.out.println("あなたの生まれた西暦は、" + (2024 - age) + "年ですね。");
+                    }
+                    if ((99 - age) < 64 & (99 - age) > 0) {
+                        System.out.println("あなたの生まれた元号は、昭和" + (99 - age) + "年ですね。");
+                        System.out.println("あなたの生まれた西暦は、" + (2024 - age) + "年ですね。");
+                    }
+                    if ((36 - age) < 31 & (36 - age) > 0) {
+                        System.out.println("あなたの生まれた元号は、平成" + (36 - age) + "年ですね。");
+                        System.out.println("あなたの生まれた西暦は、" + (2024 - age) + "年ですね。");
+                    }
+                    if ((6 - age) < 7 & (6 - age) > 0) {
+                        System.out.println("あなたの生まれた元号は、令和" + (6 - age) + "年ですね。");
+                        System.out.println("あなたの生まれた西暦は、" + (2024 - age) + "年ですね。");
+                    }
                 }
             }
         } catch (IOException e) {
